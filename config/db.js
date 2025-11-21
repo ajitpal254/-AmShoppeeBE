338f75ea2a82d308f83a7c8befdca969af5c70aa
Ajit PaL <ajitpal353@gmail.com>
2025-11-20 19:05:31 -0500

    Remove deprecated MongoDB connection options

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 const mongose = require('mongoose');
 require('colors')
-const connectDB = async () =>{
-    try{
-        const conn = await mongose.connect(process.env.MONGO_URI,{
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        })
-        console.log('Db connected '.yellow+ conn.connection.host)
+const connectDB = async () => {
+    try {
+        const conn = await mongose.connect(process.env.MONGO_URI)
+        console.log('Db connected '.yellow + conn.connection.host)
     }
     catch (e) {
-        console.error('Error:'.red+ e.message)
+        console.error('Error:'.red + e.message)
         process.exit(1)
     }
 };
