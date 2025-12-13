8bc7d63fb9d2852c30a64d02d5d983a4f90ff747
Ajit PaL <ajitpal353@gmail.com>
2025-12-13 13:22:47 -0500

    vulnerability fix

--- seeders.js
+++ seeders.js
@@ -1,59 +1,52 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const users = require('./data/user');
-const user = require('./models/user');
-const Product = require('./models/ProductModel');
-const  order = require('./models/OrderModel');
-const products = require('./data/products');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const users = require("./data/user");
+const user = require("./models/user");
+const Product = require("./models/ProductModel");
+const order = require("./models/OrderModel");
+const products = require("./data/products");
 
-const connectDB = require('./config/db');
-require('colors')
-
+const connectDB = require("./config/db");
+require("colors");
 
 dotenv.config();
 connectDB();
 
-const importData = async () =>{
-    try{
-        await order.deleteMany()
-        await Product.deleteMany()
-        await user.deleteMany()
+const importData = async () => {
+  try {
+    await order.deleteMany();
+    await Product.deleteMany();
+    await user.deleteMany();
 
-        console.log('Users to import:', users);
-        const createUser = await user.insertMany(users)
-        const adminUser = createUser[0]._id
-        const sampleData = products.map(product => {
-            return {...product, user: adminUser}
-        })
-        await Product.insertMany(sampleData)
-        console.log('Data Imported'.green.inverse)
-        process.exit()
-    }
-    catch (e) {
-        console.log(e.message)
-        process.exit(1)
-    }
+    console.log("Users to import:", users);
+    const createUser = await user.insertMany(users);
+    const adminUser = createUser[0]._id;
+    const sampleData = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+    await Product.insertMany(sampleData);
+    console.log("Data Imported".green.inverse);
+    process.exit();
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
 };
 
-const dataDestroy = async ()=>{
-
-    try{
-        await order.deleteMany()
-        await Product.deleteMany()
-        await user.deleteMany()
-        console.log('Data Destroyed'.red)
-    } catch (e) {
-        console.log(e)
-        process.exit();
-
-    }
-
+const dataDestroy = async () => {
+  try {
+    await order.deleteMany();
+    await Product.deleteMany();
+    await user.deleteMany();
+    console.log("Data Destroyed".red);
+  } catch (e) {
+    console.log(e);
+    process.exit();
+  }
 };
 
-if(process.argv[2]== "-d"){
-    dataDestroy();
+if (process.argv[2] == "-d") {
+  dataDestroy();
+} else {
+  importData();
 }
-else{
-    importData();
-}
-
